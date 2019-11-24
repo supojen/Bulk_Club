d64ffd298a9f8ec9db264194e9d96c9dab1b33af
msoliman5 <54380486+msoliman5@users.noreply.github.com>
2019-11-23 18:02:53 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Controller controller;
+    MainWindow w(&controller);
+    w.show();
+    return a.exec();
+}
