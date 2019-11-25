2133b743ea313425ef8d6fb4ddc20c1dfb853d93
metaknighttu <54918445+metaknighttu@users.noreply.github.com>
2019-11-25 15:03:56 -0800

    Add files via upload

---
+++ TRAVIS/script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $TRAVIS_OS_NAME = 'osx' ]; then
+
+    # Install some custom requirements on macOS
+    # e.g. brew install pyenv-virtualenv
+	brew update
+	brew install ruby-build
+	brew install rbenv
+	rbenv install 2.6.3
+	rbenv global 2.6.3	
+	brew install qt5
+	brew link qt5 --force
+
+else
+
+    # Install some custom requirements on Linux
+	sudo apt-get update && upgrade --yes
+	sudo apt-get install --yes build-essential qtdeclarative5-dev qt5-default sqlite3
+
+fi
