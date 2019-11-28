a86283d45a39c081bf6d6cb56dc32d478f90cc35
Adam Hurd <49463217+AdamWHurd123@users.noreply.github.com>
2019-11-27 23:03:01 -0800

    Add files via upload

--- controller.h
+++ controller.h
@@ -2,6 +2,19 @@
 #define CONTROLLER_H
 
 #include <QObject>
+#include <QString>
+#include <QDate>
+#include <QtSql>
+#include <QList>
+#include <QMap>
+#include <QFile>
+#include <QFileDialog>
+#include <QStringList>
+#include "member.h"
+#include "record.h"
+#include "commodity.h"
+#include "admin.h"
+
 
 class Controller : public QObject
 {
@@ -9,9 +22,173 @@ class Controller : public QObject
 public:
     explicit Controller(QObject *parent = nullptr);
 
+    Controller(Controller& controller);
+    ~Controller();
+
+    void createTable();
+
+    //==========================================================================================
+    // This part will change the data
+    //==========================================================================================
+    //******************************************************************************************
+    // Creating the entry
+    //******************************************************************************************
+    void createMember(int     id,
+                      QString name,
+                      QString type,
+                      QDate   date,
+                      float   spent,
+                      float   rebate);
+    void createRecord(int     member_id,
+                      QDate   date,
+                      QString item,
+                      int     quantity);
+    void createCommodity(QString item,
+                         float price,
+                         int quantity,
+                         float revenue);
+    void createInventoryTable(QString item,
+                              float price);
+
+    //******************************************************************************************
+    // deleting the entry
+    //******************************************************************************************
+    void deleteMember(int id);
+    //******************************************************************************************
+    // updateing the entry
+    //******************************************************************************************
+    void updatemember(int     id,
+                      QString name,
+                      QString type,
+                      QDate   date,
+                      float   spent,
+                      float   rebate);
+
+    void updateCommodity(QString item,
+                         float price,
+                         int quantity,
+                         float revenue);
+    void updateInventory(QString item,
+                         float price);
+
+    //==========================================================================================
+    // This part will not change the data
+    // This part is about reading the data
+    //==========================================================================================
+    //******************************************************************************************
+    // Getting the sales report according to the date
+    // Input:
+    //      QDate date:                   representing the date you want to check
+    // Return:
+    //      QMap<QString, int>& items :   <item's name, item's quantity> (According to the date)
+    //      QList<int>& regularMembers:   <regular member's id> (According to the date)
+    //      QList<int>& executiveMembers: <executive member 's id> (According to the date)
+    //      int&:                         The total revenue according to the date
+    //******************************************************************************************
+    void getSalesReportBydate(QDate               date,
+                              QMap<QString, int>& items,
+                              QList<int>&         regularMembers,
+                              QList<int>&         executiveMembers,
+                              int&                revenue);
+    //******************************************************************************************
+    // Getting the member ID that will expired by the givn date(the same month and year)
+    // Input:
+    //      QDate date:                   representing the date you want to check
+    // Return:
+    //      QList<int>& members:          <member 's ID>,
+    //                                    these are the members who's membership will expired in
+    //                                    the month and year you given
+    //******************************************************************************************
+    void getexpireMembers(QDate date, QList<int>& members);
+    //******************************************************************************************
+    // Getting the total quantity of items
+    // Return:
+    //      QMap<QString, int>& totalQuantityOfItems: <item's name, the qunatity of the item>
+    //******************************************************************************************
+    void getTotalQuantityOfItems(QMap<QString, int>& totalQuantityOfItems);
+    //******************************************************************************************
+    // Getting the total revenue of items
+    // Return:
+    //      QMap<QString, int>& totalRevenueOfItems: <item's name, the Revenue of the item>
+    //****************************************************************************************
+    void getTotalRevenueOfItems(QMap<QString, float>& totalRevenueOfItems);
+    //******************************************************************************************
+    // Getting the records according to the member
+    // Return:
+    //      QMap<QString, QList<Record*>>& membersPuchasedHistory: <member's ID, the record related to the member>
+    //******************************************************************************************
+    void getMembersPurchased(QMap<int, QList<Record*> >& membersPuchasedHistory);
+    //******************************************************************************************
+    // Getting the members' rebate
+    // Return:
+    //      QMap<int,float>& rebateOfMembers: <member's ID, the rebate of the member>
+    //******************************************************************************************
+    void getRebateByMembers(QMap<int,float>& reabateOfMembers);
+    //******************************************************************************************
+    // Getting the members' total spent
+    // Return:
+    //      QMap<int,float>& rebateOfMembers: <member's ID, the total spent of the member>
+    //******************************************************************************************
+    void getTotalSpentBymembers(QMap<int,float>& totalSpentOfMembers);
+
+
+    //==========================================================================================
+    // This function get the list of the infos, but if you change the data you get here
+    // it will not change the database
+    //==========================================================================================
+    QList<Commodity*> getCommodity();           // returns a pointer to commodities
+    QList<Admin*>     getAdmins();              // returns a pointer to admins
+    QList<Record*>    getRecords();             // returns a pointer to sales records
+    QList<Member*>    getMembers();             // returns a pointer to members
+    QMap<QString,float> getCommodityPriceList();    // returns a list of prices
+
+    QSqlQueryModel* getMembersQueryModel();     // returns the list of members as a tableview (GUI)
+    QSqlQueryModel* getRecordsQueryModel();     // returns the record of sales as a tableview (GUI)
+    QSqlQueryModel* getCommoditiesQueryModel(); // returns the list of commodities as a tableview (GUI)
+    QSqlQueryModel* getRevenueSortedById();     // returns the list of member revenue sorted by member ID (asc. order) as a tableview (GUI)
+    QSqlQueryModel* getRevenueSortedByRev();    // returns the list of member revenue sorted by revenue (asc. order) as a tableview (GUI)
+    QSqlQueryModel* SortByRevenueItems();       // returns the list of commodities sorted by revenue (asc. order) as a tableview (GUI)
+    QSqlQueryModel* SortByNameItems();          // returns the list of commodities sorted by item name (asc. order) as a tableview (GUI)
+    QSqlQueryModel* getInventoryQueryModel();   // returns the inventory list as a tableview (GUI)
+    QSqlQueryModel* getMemberUpgrades();        // returns the list of members with suggestions for account upgrade/downgrade (GUI)
+
+
+    QSqlTableModel* getMembersQueryModelWithCondition(QString condition);
+    QSqlTableModel* getRecordsQueryModelWithCondition(QString condition);
+    QSqlTableModel* getCommoditiesQueryModelWithCondition(QString condition);
+    QSqlTableModel* getMembersExpiredAttheMonth(int year,int month);
+
+    bool readRecordFile();                      // Reads a daily sales record from a file into the database. Returns true if the operation is succesful
+    bool readMemberFile();                      // Reads a list of member information from a file into the database. Returns true if the operation is successful.
+
+    float calcMemberSpent(int member_id);       // Calculates total purchases for a member. Returns the sum
+    float calcMemberRebate(int member_id);      // Calculates the membership rebate for a customer. Returns the rebate.
+
+    Commodity* getCommodityByItemName(QString item); // Searches the commodity table for a given item name. If the item is found, a pointer to that item is returned.
+                                                     // if it is not found, a null pointer is returned.
+    QSqlQueryModel* getCommoditiesQueryModelbyName(QString name);   // Searches the commodity table for a given item name. If it is found, the results are displayed as tableview (GUI)
+    Member* getMemberById(int id);              // Searches the member table for a member ID. Returns a pointer to that member if found. If not found, returns a null pointer
+
+    void setComboDate(QString combo);           // Sets the combo date (MMM/DD/YYYY) to the passed parameter
+    void getComboDate (QString &combo);         // Returns the combo date (MMM/DD/YYYY)
+
 signals:
 
 public slots:
+private:
+    QSqlDatabase      m_database;
+    QList<Member*>    m_members;
+    QList<Record*>    m_records;
+    QList<Commodity*> m_commodities;
+    QList<Admin*>     m_admins;
+    QString comboDate;         // Format: MMM/DD/YYYY
+
+    void loadMembers();
+    void loadRecords();
+    void loadCommodities();
+    void loadAdmins();
+    void loadInventory();
+
 };
 
 #endif // CONTROLLER_H
--- member.h
+++ member.h
@@ -2,6 +2,8 @@
 #define MEMBER_H
 
 #include <QObject>
+#include <QString>
+#include <QDate>
 
 class Member : public QObject
 {
@@ -9,9 +11,48 @@ class Member : public QObject
 public:
     explicit Member(QObject *parent = nullptr);
 
+    /* Getter and Setter for m_id */
+    int id() const;
+    void setId(int id);
+    /* Getter and Setter for m_name */
+    QString name() const;
+    void setName(const QString &name);
+    /* Getter and Setter for m_type */
+    QString type() const;
+    void setType(const QString &type);
+    /* Getter and Setter for m_date */
+    QDate date() const;
+    void setDate(const QDate &date);
+    /* Getter and Setter for m_spent */
+    float spent() const;
+    void setSpent(float spent);
+    /* Getter and Setter for m_rebate */
+    float rebate() const;
+    void setRebate(float rebate);
+
+    /* Getter and Setter for m_upgrade */
+    QString upgrade() const;
+    void setUpgrade(QString upgrade);
+
+
 signals:
+    void idChanged();
+    void nameChanged();
+    void typeChanged();
+    void dateChanged();
+    void spentChanged();
+    void rebatChanged();
 
 public slots:
+
+private:
+    int     m_id;
+    QString m_name;
+    QString m_type;
+    QDate   m_date;
+    float   m_spent;
+    float   m_rebate;
+    QString m_upgrade;
 };
 
 #endif // MEMBER_H
