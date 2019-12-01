9fd3899a9d2dba9f8a06272d6c2020e574384e11
metaknighttu <54918445+metaknighttu@users.noreply.github.com>
2019-11-30 23:51:36 -0800

    Update main-script.sh

--- TRAVIS/main-script.sh
+++ TRAVIS/main-script.sh
@@ -2,31 +2,29 @@
 
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
 
-    # Install some custom requirements on macOS
-    # e.g. brew install pyenv-virtualenv
+# Install some custom requirements on macOS
+# e.g. brew install pyenv-virtualenv
 	qmake -spec macx-xcode bulkClub.pro
 	xcodebuild -list -project bulkClub.xcodeproj
 	xcodebuild -scheme bulkClub build
 
 elif [ $TRAVIS_OS_NAME = 'linux' ]; then
 
-    # Install some custom requirements on Linux
+# Install some custom requirements on Linux
 	qmake bulkClub.pro
 	make
 
 elif [ $TRAVIS_OS_NAME = 'windows' ]; then
 
-	#Install custom requirements for Windows
+#Install custom requirements for Windows
 	echo "Windows Placeholder"
-	#qmake bulkClub.pro
+#qmake bulkClub.pro
 	/c/tools/qtcreator/bin/qtpromaker.exe bulkClub.pro
 	ls -lah
-	#nmake bulkClub.pro
-	qmake bulkClub.pro
-	mingw32-make Bulk_Club.pro
-	#
-	#
-
+#nmake bulkClub.pro
+#qmake bulkClub.pro
+#mingw32-make Bulk_Club.pro
+	
 else 
 
 echo "No Compatible OS Specified"
