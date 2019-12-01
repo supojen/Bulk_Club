2d0c1c6120fe4b1e215d89f6d0151967955d32fe
metaknighttu <54918445+metaknighttu@users.noreply.github.com>
2019-11-30 21:05:36 -0800

    Update main-script.sh

--- TRAVIS/main-script.sh
+++ TRAVIS/main-script.sh
@@ -19,7 +19,7 @@ elif [ $TRAVIS_OS_NAME = 'windows' ]; then
 	#Install custom requirements for Windows
 	echo "Windows Placeholder"
 	#qmake bulkClub.pro
-	/tools/qtcreator/bin/qmake.exe bulkClub.pro -r -o makefile
+	C:/tools/qtcreator/bin/qmake.exe bulkClub.pro -r -o makefile
 	mingw32-make makefile
 	#
 	#
