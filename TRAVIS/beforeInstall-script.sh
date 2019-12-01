ddd03088667bc39d92e32c28cd7231272d7d072d
metaknighttu <54918445+metaknighttu@users.noreply.github.com>
2019-11-30 20:55:26 -0800

    Update beforeInstall-script.sh

--- TRAVIS/beforeInstall-script.sh
+++ TRAVIS/beforeInstall-script.sh
@@ -24,8 +24,8 @@ elif [ $TRAVIS_OS_NAME = 'windows' ]; then
 	#Install custom requirements for Windows
 	#echo "Windows Placeholder"
 	cd
-	chocolatey upgrade chocolatey
-	choco upgrade all
+	#chocolatey upgrade chocolatey
+	#choco upgrade all
 	#choco install qt-sdk-windows-x64-mingw_opengl_seh
 	choco install qtcreator
 	#
